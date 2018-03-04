5a09db3ec0bced401f7407499924ed2393645bc3
Brian Manthos <manthos@msn.com>
2018-03-03 16:39:22 -0800

    frmMain is now a topmost window

--- UltimateFishBot/Forms/frmMain.Designer.cs
+++ UltimateFishBot/Forms/frmMain.Designer.cs
@@ -175,6 +175,7 @@
             this.MaximizeBox = false;
             this.Name = "frmMain";
             this.Text = "UltimateFishBot - v 4.1";
+            this.TopMost = true;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
             this.Load += new System.EventHandler(this.frmMain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
